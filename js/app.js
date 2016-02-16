5096a03437752dea9e1ea62a8050e6c2e886881c
vaidy shanmugam <vshanmugam@housecanary.com>
2016-02-16 15:22:48 -0800

    moving js apart from html

---
+++ js/app.js
@@ -0,0 +1,10 @@
+angular.module('myApp', [])
+    .controller('myController', function ($scope, $timeout) {
+        var updateClock = function () {
+            $scope.clock = new Date();
+            $timeout(function () {
+                updateClock()
+            }, 1000);
+        };
+        updateClock();
+    });
